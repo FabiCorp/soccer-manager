66390349c75ced8da84c79ff52f4cc976bce3e20
Fabian Wenzel <wenzelfabian1996@gmail.com>
2020-08-20 10:37:09 +0200

    inital commit (spring)

---
+++ src/main/java/com/manager/soccer/SoccerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.manager.soccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerManagerApplication.class, args);
+	}
+
+}
