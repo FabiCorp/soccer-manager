c3c0548f8c2e608f027501609b2c5ceebfd5e502
Fabian Wenzel <wenzelfabian1996@gmail.com>
2020-08-29 10:53:47 +0200

    switch to material ui design

---
+++ frontend/soccer-manager/src/NavTable/ListDataTuple.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import SwapHorizontalCircleIcon from '@material-ui/icons/SwapHorizontalCircle';
+
+const listDataTuple = [
+    {
+        name: "Start",
+        icon: <HomeIcon/>
+    }, {
+        name: "Transfermarkt",
+        icon: <SwapHorizontalCircleIcon/>
+    },
+];
+
+export default listDataTuple; 
--- frontend/soccer-manager/src/index.js
+++ frontend/soccer-manager/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PermanentDrawer from './NavTable/PermanantDrawer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PermanentDrawer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
